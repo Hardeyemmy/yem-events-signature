996fcb3e446764ead543afcc7c13c83c527fc0b2
Hardeyemmy <iiadeyemi307@gmail.com>
2026-07-30 22:46:56 +0100

    working circle avatar color

--- lib/features/auth/presentation/Pages/admin_page.dart
+++ lib/features/auth/presentation/Pages/admin_page.dart
@@ -311,7 +311,7 @@ class _UsersTab extends ConsumerWidget {
             child: Row(
               children: [
                 CircleAvatar(
-                  backgroundColor: const Color(0xFFF2F0FF),
+                  backgroundColor: const Color(0xFFF2F0FF).withValues(),
                   child: Text(
                     (user['displayName'] ?? user['email'] ?? '?')[0]
                         .toUpperCase(),
